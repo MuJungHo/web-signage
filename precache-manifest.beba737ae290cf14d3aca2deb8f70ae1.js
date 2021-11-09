189e8b0ebdd0c5da0d920a170064f4af7341faa9
MuJungHo <ande1218@gmail.com>
2021-11-09 16:20:52 +0800

    Updates

---
+++ precache-manifest.beba737ae290cf14d3aca2deb8f70ae1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c8b3cc562b59dd637cf9a6bd286f6f88",
+    "url": "./index.html"
+  },
+  {
+    "revision": "ffca04d9e346559d89fe",
+    "url": "./static/css/main.bea51ce1.chunk.css"
+  },
+  {
+    "revision": "7398dd53b29c0def6251",
+    "url": "./static/js/2.824d4555.chunk.js"
+  },
+  {
+    "revision": "83866a54784fff4313d5e231d70b1042",
+    "url": "./static/js/2.824d4555.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ffca04d9e346559d89fe",
+    "url": "./static/js/main.e5a123dc.chunk.js"
+  },
+  {
+    "revision": "a76c95ec4dbc3a6f77ea",
+    "url": "./static/js/runtime-main.ac3e5bc3.js"
+  }
+]);
--- src/index.js
+++
@@ -1,6 +0,0 @@
-import 'react-app-polyfill/ie11'
-import React from 'react'
-import { render } from 'react-dom'
-import App from './App'
-
-render(<App />,document.getElementById('root'))
