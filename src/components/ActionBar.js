fb10ecf3c73ad271e5643215a7a9d0c3934c97fa
MuJungHo <ande1218@gmail.com>
2021-11-11 15:01:08 +0800

    複製區塊

--- src/components/ActionBar.js
+++ src/components/ActionBar.js
@@ -5,12 +5,9 @@ import DeleteIcon from '@material-ui/icons/Delete';
 import FileCopyIcon from '@material-ui/icons/FileCopy';
 import moment from 'moment'
 export default ({ layers, setLayers, activeLayerID, setActiveLayerID }) => {
+  const activeLayer = layers.find(layer => layer.id === activeLayerID) || {}
   return (
     <Paper style={{ width: 960 }}>
-      <IconButton color="primary" component="span"
-        onClick={() => { }}>
-        <FileCopyIcon />
-      </IconButton>
       <IconButton color="primary" component="span"
         disabled={!activeLayerID}
         style={{ float: 'right' }}
@@ -19,6 +16,14 @@ export default ({ layers, setLayers, activeLayerID, setActiveLayerID }) => {
           setActiveLayerID({})
         }}>
         <DeleteIcon />
+      </IconButton>
+      <IconButton color="primary" component="span"
+        disabled={!activeLayerID}
+        style={{ float: 'right' }}
+        onClick={() => {
+          setLayers([...layers, { ...activeLayer, id: moment().unix(), left: activeLayer.left + 10, top: activeLayer.top + 10 }])
+        }}>
+        <FileCopyIcon />
       </IconButton>
       <IconButton color="primary" component="span"
         disabled={!activeLayerID}
