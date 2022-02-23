7687a51ca4a89055ecee44471a6dab0ae46574fd
MuJungHo <ande1218@gmail.com>
2022-02-23 11:15:45 +0800

    [新增] 匯出 content 資料

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,8 +4,19 @@ import {
 } from '@material-ui/core'
 import Player from './Player'
 import PlayArrowIcon from '@material-ui/icons/PlayArrow';
+import SystemUpdateAlt from '@material-ui/icons/SystemUpdateAlt';
+import moment from 'moment'
 export default ({ layers, board }) => {
   const [isDialogOpen, setDialogOpen] = React.useState(false)
+  const exportJsonFile = () => {
+    let dataStr = JSON.stringify(layers);
+    let dataUri = 'data:application/json;charset=utf-8,' + encodeURIComponent(dataStr);
+
+    let linkElement = document.createElement('a');
+    linkElement.setAttribute('href', dataUri);
+    linkElement.setAttribute('download', `data_${moment().unix()}.json`);
+    linkElement.click();
+  }
   return (
     <div style={{
       height: 80,
@@ -19,7 +30,8 @@ export default ({ layers, board }) => {
         <img src="./assets/github.png" style={{ width: 21, height: 21, borderRadius: '50%' }} />
       </a>
       <div style={{ flex: 1 }}></div>
-      <Button variant="contained" color="primary" onClick={() => setDialogOpen(true)}><PlayArrowIcon />Play</Button>
+      <Button style={{ marginRight: 20 }} variant="contained" color="primary" onClick={exportJsonFile}><SystemUpdateAlt style={{ marginRight: 10 }} />Export</Button>
+      <Button variant="contained" color="primary" onClick={() => setDialogOpen(true)}><PlayArrowIcon style={{ marginRight: 10 }}/>Play</Button>
       <Player isDialogOpen={isDialogOpen} setDialogOpen={setDialogOpen} layers={layers} board={board} />
     </div>
   )
