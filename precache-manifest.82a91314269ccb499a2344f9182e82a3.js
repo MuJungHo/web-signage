9b032bb8a69763ea39458af9d7ff32b4a4cf291f
MuJungHo <ande1218@gmail.com>
2022-03-16 15:50:05 +0800

    Updates

--- precache-manifest.82a91314269ccb499a2344f9182e82a3.js
+++ precache-manifest.82a91314269ccb499a2344f9182e82a3.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "14c4144e39afbe73a31cd4d22c34cd86",
+    "revision": "8787e6f45091e4e1b604179e06fa92ad",
     "url": "/web-signage/index.html"
   },
   {
-    "revision": "b005bde11a825cbf6e09",
+    "revision": "c0f220d476865cc1ebd9",
     "url": "/web-signage/static/css/main.469c52e7.chunk.css"
   },
   {
-    "revision": "96761e2802010355f784",
-    "url": "/web-signage/static/js/2.1f57fe26.chunk.js"
+    "revision": "cd0c796d025c33369917",
+    "url": "/web-signage/static/js/2.523c4f6a.chunk.js"
   },
   {
     "revision": "b01a6a39c691f0528be2e7f5ec188c86",
-    "url": "/web-signage/static/js/2.1f57fe26.chunk.js.LICENSE.txt"
+    "url": "/web-signage/static/js/2.523c4f6a.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "b005bde11a825cbf6e09",
-    "url": "/web-signage/static/js/main.8df70b5a.chunk.js"
+    "revision": "c0f220d476865cc1ebd9",
+    "url": "/web-signage/static/js/main.4859e93d.chunk.js"
   },
   {
     "revision": "9ec9cd52f3f7d58ac9df",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/web-signage/precache-manifest.c9a917adddc557e9d74e12e68c296f77.js"
+  "/web-signage/precache-manifest.82a91314269ccb499a2344f9182e82a3.js"
 );
 
 self.addEventListener('message', (event) => {
